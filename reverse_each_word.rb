85954c034c2321c50951959d087676fe7307a2cf
MJ Johnson <mjohnson4685@gmail.com>
2020-09-13 19:25:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 
-greeting = ["Hello there, and how are you?"]
 
 def reverse_each_word(greeting)
-  greeting.reverse do |hello|
-    puts "#{hello}"
+  greeting do |hello|
+    puts "#{hello}".reverse
   end
 end
